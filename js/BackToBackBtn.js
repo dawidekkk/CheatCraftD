b6e7be0f16670ba898bf837c23c76f7314868d60
Dawid Czarnecki <85833530+dawidekkk@users.noreply.github.com>
2022-03-31 12:14:09 +0200

    backToTopBtn

--- js/BackToBackBtn.js
+++ js/BackToBackBtn.js
@@ -1,1 +1,25 @@
 'use strict'
+
+const backBtn = document.querySelector('.back-to-top');
+
+const scroll = () => {
+  if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    backBtn.classList.add('show-btn')
+  } else if(document.body.scrollTop === 0 || document.documentElement.scrollTop === 0) {
+    backBtn.classList.add('none')
+    backBtn.classList.remove('show-btn')
+  }
+}
+
+window.onscroll = () => {
+  scroll();
+}
+
+const backToTop = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+backBtn.addEventListener("click", backToTop);
+
+
