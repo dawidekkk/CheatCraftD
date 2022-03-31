0573da8fcb388f1c95107ad8674fc837f90ea297
Dawid Czarnecki <85833530+dawidekkk@users.noreply.github.com>
2022-03-31 14:13:22 +0200

    before btn-price

--- js/Input.js
+++ js/Input.js
@@ -1,5 +1,7 @@
 'use strict'
 
+// CENA BUTTONOW!!!
+
 let sponsorInput = document.getElementById('sponsor-input')
 let sponsorInputValue = document.getElementById('sponsor-value')
 let superInput = document.getElementById('super-input')
@@ -11,7 +13,14 @@ let turboInputValue = document.getElementById('turbo-value')
 let cheatInput = document.getElementById('cheat-input')
 let cheatInputValue = document.getElementById('cheat-value')
 
+let btn = document.querySelectorAll('.btn-price')
+
+let sms = [5.50, 6.88, 8.52, 10.17, 11, 12.93, 14.58, 16.22, 17.6, 19.25, 20,9, 22, 23.65, 25.3, 27.5, 28.6, 29.98, 31.35, 33, 55.01]
+let psc = [5, 6.25, 7.75, 9.25, 10, 11.75, 13.25, 14.75, 16, 17.5, 19, 20, 21.5, 23, 25, 26, 27.25, 28.5, 30, 50]
+let przelew = [5, 6.25, 7.75, 9.25, 10, 11.75, 13.25, 14.75, 16, 17.5, 19, 20, 21.5, 23, 25, 26, 27.25, 28.5, 30, 50]
+
 sponsorInput.addEventListener('input', function() {
+
   let val = sponsorInputValue.textContent = this.value;
   if(val == 100) {
     sponsorInputValue.textContent = 'zawsze.';
