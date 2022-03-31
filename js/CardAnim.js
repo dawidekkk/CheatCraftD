f465ad4d40f88e24774d71aa6bc15346e1a83034
Dawid <dawidekczarnecki@gmail.com>
2022-03-31 19:18:55 +0200

    CardAnim working on

---
+++ js/CardAnim.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const card = document.querySelector('.card-miecz');
+
+// card.addEventListener('mouseenter', () => {
+//   // card.classList.add('test');
+//   console.log('enter');
+//   card.classList.toggle('test')
+// })
+
+// card.addEventListener('mouseleave', () => {
+//     // card.classList.remove('test')
+//     card.classList.toggle('testR')
+// })
