ccb34bb57ca809d5fa8a298c065f68ed63f73068
Dawid Czarnecki <85833530+dawidekkk@users.noreply.github.com>
2022-04-03 23:12:28 +0200

    animation card done

--- js/CardAnim.js
+++ js/CardAnim.js
@@ -1,31 +1,86 @@
 'use strict';
 
-// const card = document.querySelector('.card-miecz');
-const card = document.querySelector('.card-miecz');
+const cardMiecz = document.querySelector('.card-miecz')
+const cardLuk = document.querySelector('.card-luk')
+const cardKilof = document.querySelector('.card-kilof')
+const cardSponsor = document.querySelector('.card-sponsor')
+const cardSuper = document.querySelector('.card-super-vip')
+const cardVip = document.querySelector('.card-vip')
+const cardUnban = document.querySelector('.card-unban')
+const cardTurbo = document.querySelector('.card-turbo-drop')
+const cardCheat = document.querySelector('.card-cheat-case')
+const cardColorful = document.querySelector('.card-colorful')
+const cardGreen = document.querySelector('.card-green')
+const cardPink = document.querySelector('.card-pink')
 
-card.addEventListener('mouseenter', () => {
-  card.classList.add('testIn')
-  card.classList.remove('testOut')
-})
+const animIn = obj => {
+  obj.addEventListener("mouseenter", () => {
+    obj.classList.add("cardAnimIn")
+    obj.classList.remove("cardAnimOut")
+  })
+}
+
+const animOut = obj => {
+  obj.addEventListener("mouseleave", () => {
+    obj.classList.add("cardAnimOut")
+    obj.classList.remove("cardAnimIn")
+  })
+}
+
+animIn(cardMiecz);
+animIn(cardLuk);
+animIn(cardKilof);
+animIn(cardSponsor);
+animIn(cardSuper);
+animIn(cardVip);
+animIn(cardUnban);
+animIn(cardTurbo);
+animIn(cardCheat);
+animIn(cardColorful);
+animIn(cardGreen);
+animIn(cardPink);
+
+animOut(cardMiecz);
+animOut(cardLuk);
+animOut(cardKilof);
+animOut(cardSponsor);
+animOut(cardSuper);
+animOut(cardVip);
+animOut(cardUnban);
+animOut(cardTurbo);
+animOut(cardCheat);
+animOut(cardColorful);
+animOut(cardGreen);
+animOut(cardPink);
 
 
-
-// card.addEventListener('mouseleave', () => {
-//     // card.classList.remove('test')
-//     card.classList.toggle('testR')
+// card.addEventListener('mouseenter', () => {
+//   card.classList.add('cardAnimIn')
+//   card.classList.remove('cardAnimOut')
 // })
 
-// card.addEventListener('mouseenter', function() {
-//   card.classList.add('cardShadow');
-//   console.log('enter');
+// card.forEach(e => {
+//   e.addEventListener('mouseenter', function(e) {
+//     e.classList.add('cardAnimIn')
+//     e.classList.remove('cardAnimOut')
+//   })
 // })
 
-// setTimeout(() => {
+// const animIn = (obj) => {
+//   itemCards.forEach(el => el.addEventListener("mouseenter", function() {
+//     itemCards.forEach(el => el.classList.add("cardAnimIn"));
+//     itemCards.forEach(el => el.classList.remove("cardAnimOut"))
+//   }))
+// }
+
+// animIn(itemCards);
+
+// card.forEach(e => {
   
-// }, 1000)
+// })
 
-card.addEventListener('mouseleave', function() {
-  card.classList.add('testOut')
-  card.classList.remove('testIn')
-})
+// card.addEventListener('mouseleave', function() {
+//   card.classList.add('cardAnimOut')
+//   card.classList.remove('cardAnimIn')
+// })
 
