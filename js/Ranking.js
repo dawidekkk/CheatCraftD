3265fdcac30a59974a761afabf1e8e3559907a72
Dawid Czarnecki <85833530+dawidekkk@users.noreply.github.com>
2022-04-05 19:23:23 +0200

    ranking position

--- js/Ranking.js
+++ js/Ranking.js
@@ -1,59 +1,25 @@
 'use strict';
 
-const testBtn = document.querySelector('.testBtn')
-
-const toggleList = (btn, list) => {
+const toggleList = (btn, list, crown) => {
   const btns = document.querySelector(`${btn}`);
   const showList = document.querySelector(`${list}`);
+  const crownHide = document.querySelector(`${crown}`)
 
   btns.addEventListener('click', () => {
     showList.classList.toggle('block')
+    
     if(showList.classList.contains('block')) {
       btns.textContent = 'Załaduj mniej'
     } else {
       btns.textContent = 'Zaladuj wiecej'
     }
+
+    crownHide.classList.toggle('hide')
   })
 }
 
-// const markup = `
-//   <table class="table intersection-anim" data-delay=".3s">
-//     <tbody>
-//       <tr class="table-row">
-//         <td>4.</td>
-//         <td><img src="./style/img/face.png" alt="Ikonka heada"></td>
-//         <td>{ nick }</td>
-//         <td>{ punkty }</td>
-//       </tr>
-//     </tbody>
-//   </table>
-// `
+toggleList('.toggle-btn_topka_rankingu', '.show-list_topka_rankingu', '.crown-hide-first');
+toggleList('.toggle-btn_topka_gildii', '.show-list_topka_gildii', '.test');
+// toggleList('.toggle-btn_topka_zabojcow', '.show-list_topka_zabojcow');
+// toggleList('.toggle-btn_topka_koxy', '.show-list_topka_koxy');
 
-// const addMarkup = (e) => {
-//   testBtn.addEventListener("click", (e) => {
-//     e.insertAdjacentHTML("afterend", markup);
-//   })
-// }
-
-// addMarkup(markup);
-
-toggleList('.toggle-btn_topka_rankingu', '.show-list_topka_rankingu');
-toggleList('.toggle-btn_topka_gildii', '.show-list_topka_gildii');
-toggleList('.toggle-btn_topka_zabojcow', '.show-list_topka_zabojcow');
-toggleList('.toggle-btn_topka_koxy', '.show-list_topka_koxy');
-
-// const toggleList = (list, btn) => {
-//   const showList = document.querySelector(list)
-//   const activatelist = document.querySelector(btn);
-
-//   activatelist.addEventListener('click', () => {
-//     showList.classList.toggle('block');
-//     if(showList.classList.contains('block')) {
-//       activatelist.textContent = 'Zmniejsz listę.'
-//     } else {
-//       activatelist.textContent = 'Załaduj więcej.'
-//     }
-//   })
-// }
-
-// toggleList('.show-list_topka_rankingu', '.toggle-btn');
